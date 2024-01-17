7b2328d4c2a23499d597f8a493ded129013620d1
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-01-17 13:18:12 +0200

    change read_csv function and generation of embeddings

--- show_suggester_ai.py
+++ show_suggester_ai.py
@@ -3,6 +3,7 @@ from openai import OpenAI
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import pickle
 
 
 load_dotenv()
@@ -23,27 +24,34 @@ def get_favorite_tv_shows(shows_list):
         return matched_shows
     return None
 
-def extract_description(file_path):
+def read_csv_file(file_path):
     try:
-        df = pd.read_csv(file_path)
-        descriptions = df['Description'].tolist()
-        return descriptions
+        tv_shows = pd.read_csv(file_path)
+        return tv_shows
     except Exception as e:
         print(f"Error reading CSV file: {e}")
 
 
 
-def generate_embeddings(descriptions, client, model="text-embedding-ada-002"):
-    with open('embeddings.txt', 'a') as file:  # Open file in append mode
-        for description in descriptions:
-            response = client.embeddings.create(
-                input=description,
-                model=model
-            )
-            embedding = response.data[0].embedding
-            file.write(str(embedding) + '\n')  # Convert embedding to string and write to file
+def generate_embeddings(tv_shows, client, model="text-embedding-ada-002"):
+    embeddings_dict = {}
+    titles = tv_shows['Title'].tolist()
+    descriptions = tv_shows['Description'].tolist()
+    counter = 0
+    for index, description in enumerate(descriptions):
+        response = client.embeddings.create(
+            input=description,
+            model=model
+        )
+        embedding = response.data[0].embedding
+        embeddings_dict[titles[index]] = embedding
+        print(counter)
+        counter +=1
 
-
+        
+    
+    with open('embeddings.pkl', 'wb') as file:  # Open file in binary write mode
+        pickle.dump(embeddings_dict, file)
 
 
 if __name__ == "__main__":
@@ -64,9 +72,10 @@ if __name__ == "__main__":
                 print("Please enter at least 2 TV shows and don't leave any empty spaces between commas.\n")   
 
     client = OpenAI(api_key=os.getenv('OPENAI_API_KEY')) #Jeremy, oublie pas dde rentrer ton API key dans un .env :)
-    if not os.path.exists('./embeddings.txt'):
-        descriptions = extract_description('./imdb_tvshows - imdb_tvshows.csv') #Monte ca en haut et passe le dans la fonction !
-        generate_embeddings(descriptions, client)
+    if not os.path.exists('./embeddings.pkl'):
+        print('enter')
+        tv_shows = read_csv_file('./imdb_tvshows - imdb_tvshows.csv') #Monte ca en haut et passe le dans la fonction !
+        generate_embeddings(tv_shows, client)
 
    
 
