38ac90be08c8a26732e2b541865fde85f57cb55a
Ethan Fajnkuchen <102037640+EthanFajnkuchen@users.noreply.github.com>
2024-01-17 12:12:27 +0200

    change main function

--- show_suggester_ai.py
+++ show_suggester_ai.py
@@ -7,45 +7,21 @@ import os
 
 load_dotenv()
 
-def ask_user():
 
-    flag = True
-
-    while flag :
-        # Prompt the user for input
-        user_input = input("Which TV shows did you love watching? Separate them by a comma. Make sure to enter more than 1 show: \n")
-        shows_list = user_input.split(",")
-
-        #Check if the user entered 3 TV shows
-        if len(shows_list) < 2:
-            print("Please enter at least 2 TV shows.\n")
-
-        #Check if the user entered whitespaces or empty strings
-        elif not all(show.strip() for show in shows_list):
-            print("Please enter non-empty TV shows.\n")
-
-        else :
-            flag = False
-
+def ask_user(user_input):
+    shows_list = user_input.split(",")
+    # Check if the user entered more than 1 show or if there is an empty string/whitespaces only
+    if len(shows_list) < 2 or not all(show.strip() for show in shows_list):
+        return None
     return shows_list
 
-
-def get_favorite_tv_shows():
-    # Sample list of TV shows for fuzzy matching
+def get_favorite_tv_shows(shows_list):
     known_shows = ["Game of Thrones", "Lupin", "The Witcher", "Breaking Bad", "Stranger Things"]
 
-    while True:
-        shows = ask_user()
-
-        matched_shows = [process.extractOne(show, known_shows)[0] for show in shows]
-        confirmation = input(f"Just to make sure, do you mean {', '.join(matched_shows)}? (y/n) \n")
-
-        if confirmation.lower() == 'y':
-            print("Great! Generating recommendations...")
-            return matched_shows
-        else:
-            print("Sorry about that. Let's try again, please make sure to write the names of the TV shows correctly.\n")
-    
+    if shows_list:
+        matched_shows = [process.extractOne(show, known_shows)[0] for show in shows_list]
+        return matched_shows
+    return None
 
 def extract_description(file_path):
     try:
@@ -57,8 +33,7 @@ def extract_description(file_path):
 
 
 
-def generate_embeddings(file_path, client, model="text-embedding-ada-002"):
-    descriptions = extract_description(file_path)
+def generate_embeddings(descriptions, client, model="text-embedding-ada-002"):
     with open('embeddings.txt', 'a') as file:  # Open file in append mode
         for description in descriptions:
             response = client.embeddings.create(
@@ -72,8 +47,27 @@ def generate_embeddings(file_path, client, model="text-embedding-ada-002"):
 
 
 if __name__ == "__main__":
-   client = OpenAI(api_key=os.getenv('OPENAI_API_KEY')) #Jeremy, oublie pas dde rentrer ton API key dans un .env :)
-   generate_embeddings('./imdb_tvshows - imdb_tvshows.csv', client)
+    while True:
+        user_input = input("Which TV shows did you love watching? Separate them by a comma. Make sure to enter more than 1 show: \n")
+        # user_inputs.append(user_input)
+        shows_list = ask_user(user_input)
+        favorite_shows = get_favorite_tv_shows(shows_list)
+        if favorite_shows:
+            print(f"Just to make sure, do you mean {', '.join(favorite_shows)}? (y/n) ")
+            confirmation = input()
+            if confirmation.lower() == 'y':
+                print("Great! Generating recommendations...")
+                break
+            else:
+                print("Sorry about that. Let's try again, please make sure to write the names of the TV shows correctly.\n")
+        else:
+                print("Please enter at least 2 TV shows and don't leave any empty spaces between commas.\n")   
+
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY')) #Jeremy, oublie pas dde rentrer ton API key dans un .env :)
+    if not os.path.exists('./embeddings.txt'):
+        descriptions = extract_description('./imdb_tvshows - imdb_tvshows.csv') 
+        generate_embeddings(descriptions, client)
+
    
 
 # Example usage
